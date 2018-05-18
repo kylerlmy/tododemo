5dccde26f2dbbc40ca9fafd3bddf236f81f35f46
kyle <kylerlimy@gmail.com>
2018-05-18 08:55:02 +0800

    solve units error

--- src/AspNetCore.ToDo.UnitTests/TodoItemServiceShould.cs
+++ src/AspNetCore.ToDo.UnitTests/TodoItemServiceShould.cs
@@ -1,13 +1,17 @@
 using System;
+using AspNetCore.ToDo.Data;
+using AspNetCore.ToDo.Models;
+using AspNetCore.ToDo.Services;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
+using  System.Threading.Tasks;
 namespace AspNetCore.ToDo.UnitTests {
     public class TodoItemServiceShould {
         [Fact]
-        public void Task AddNewItem () {
+        public async  Task AddNewItem () {
             var options = new DbContextOptionsBuilder<ApplicationDbContext> ()
-                .UseInMomeryDatabase (databaseName: "Test_AddItem").Options;
+                .UseInMemoryDatabase (databaseName: "Test_AddItem").Options;
 
             using (var inMemoryContext = new ApplicationDbContext (options)) {
                 var service = new TodoItemService (inMemoryContext);
@@ -15,14 +19,14 @@ namespace AspNetCore.ToDo.UnitTests {
                     Id = "fake-000",
                     UserName = "fake@fake"
                 };
-                await service.AddItemAsync (new NewTodoItem { Tile = "Tesing?" }, fakeUser);
+                await service.AddItemAsync (new NewTodoItem { Title = "Testing?" }, fakeUser);
             }
 
             using (var inMemoryContext = new ApplicationDbContext (options)) {
                 Assert.Equal (1, await inMemoryContext.Items.CountAsync ());
                 var item = await inMemoryContext.Items.FirstAsync ();
                 Assert.Equal ("Testing?", item.Title);
-                Assert.Equal (false, item.IsDone);
+                Assert.False (item.IsDone);
                 Assert.True (DateTimeOffset.Now.AddDays (3) - item.DueAt < TimeSpan.FromSeconds (1));
             }
 
